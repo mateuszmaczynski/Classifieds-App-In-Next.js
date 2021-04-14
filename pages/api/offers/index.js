f1f35f0222d521f52d2b2599cc12cfea07a1d72a
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-04-14 23:46:52 +0200

    add endpoint to sending data from form to db

--- pages/api/offers/index.js
+++ pages/api/offers/index.js
@@ -1,6 +1,20 @@
 import getRecentOffers from 'services/offers/getRecent';
+import createOffer from 'services/offers/create';
 
 export default async (req, res) => {
-  const offers = await getRecentOffers(10);
-  res.status(200).json(offers);
+  switch (req.method) {
+    case 'GET': {
+      const offers = await getRecentOffers(4);
+      res.status(200).json(offers);
+      break;
+    }
+    case 'POST': {
+      const payload = req.body;
+      const offer = await createOffer(payload);
+      res.status(200).json({status: 'created', offer});
+      break;
+    }
+    default:
+      res.status(400);
+  }
 };
