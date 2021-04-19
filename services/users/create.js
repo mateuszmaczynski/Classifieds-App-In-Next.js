29193c0548e79cf19506f286b54336d442c91ed0
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-04-19 22:29:57 +0200

    add user registration - endpoint and function to creating data in table

---
+++ services/users/create.js
@@ -0,0 +1,32 @@
+import db from 'services/airtableClient';
+import Joi from 'joi';
+import crypto from 'crypto';
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  fullName: Joi.string().min(3).max(60).required(),
+  password: Joi.string().min(8).max(30).required()
+});
+
+const create = async (payload) => {
+  const { email, fullName, password } = await schema.validateAsync(payload);
+  const passwordSalt = crypto.randomBytes(16).toString('hex');
+  const passwordHash = crypto
+    .pbkdf2Sync(password, passwordSalt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  const user = await db('users').create([
+    {
+      fields: {
+        email,
+        fullName,
+        passwordSalt,
+        passwordHash,
+        role: 'regular'
+      }
+    }
+  ]);
+
+  return user;
+}
+
+export default create;
